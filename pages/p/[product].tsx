2c100468a60c67c111c5345fbf6f14da258ed0c0
aboumegouass <aboumegouass@gmail.com>
2021-12-12 19:44:43 +0100

    k

--- pages/p/[product].tsx
+++ pages/p/[product].tsx
@@ -1,25 +1,18 @@
 import Layout from '@/components/Layout/HomeLayout'
 import { ReactElement } from "react";
 import 'react-slideshow-image/dist/styles.css'
-import useSWR from "swr";
-import Loading from '@/components/Loading'
-import { message } from 'antd'
-import PropTypes from "prop-types";
-
 // 
-function Single({ query }) {
+function Single() {
 
-  const { data: ProductData }: any = useSWR(`api/product/${query.product}`)
-  if (!ProductData) { message.loading('يرجى الإنتظار...') }
+  //const { data: ProductData }: any = useSWR(`api/product/${query.product}`)
+  //if (!ProductData) { message.loading('يرجى الإنتظار...') }
 
   return (
     <>
-      {!ProductData && <Loading />}
-      {ProductData &&
         <div className="timwoork-single">
-          kjhshskjd {query.product}
+          kjhshskjd
         </div>
-      }
+      
     </>
   );
 }
@@ -31,10 +24,3 @@ Single.getLayout = function getLayout(page: any): ReactElement {
   )
 }
 export default Single;
-
-Single.getInitialProps = ({ query }) => {
-    return { query }
-}
-Single.propTypes = {
-  query: PropTypes.any,
-};
