7a50efdc5574064627c940799a02983847c6a8b1
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-01-30 15:29:09 +0100

    Y

--- config/pusher.tsx
+++ config/pusher.tsx
@@ -2,7 +2,7 @@ import Pusher from 'pusher-js'
 import Cookies from 'js-cookie'
 
 const token = Cookies.get('token')
-export const pusher = new Pusher('510f53f8ccb3058a96fc', {
+export const pusher = new Pusher('a00614632e45ad3d49ff', {
     cluster: 'eu',
     authEndpoint: 'https://api.icoursat.com/api/broadcasting/auth',
     auth: token ? {
