bf9822437398417e290801c58f2b0eea170cd9e5
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-03-10 11:04:38 +0100

    de

--- components/PostsAside.tsx
+++ components/PostsAside.tsx
@@ -81,7 +81,7 @@ function PostsAside({ PostData, title, colNumber, isError, linkURL }): ReactElem
                                     size="small"
                                     title={e.title}
                                     author={e.profile_seller && (e.profile_seller.profile.first_name + ' ' + e.profile_seller.profile.last_name)}
-                                    rate={e.ratings_avg_rating.toPrecision(1)}
+                                    rate={e.ratings_avg_rating}
                                     username={e.profile_seller && e.profile_seller.profile.user.username}
                                     price={e.price}
                                     slug={e.slug}
