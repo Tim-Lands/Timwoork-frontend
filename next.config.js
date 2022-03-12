9d6775d02e01c1ce347a9222b808766d8969f602
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-03-12 14:20:54 +0100

    Add users Page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-      domains: ['api.icoursat.com'],
+      domains: ['api.icoursat.com', 'lh3.googleusercontent.com'],
     },
   }
--- pages/tw-admin/users/index.tsx
+++ pages/tw-admin/users/index.tsx
@@ -1,37 +1,114 @@
-import { ReactElement, useState } from "react";
-import DashboardLayout from "@/components/Layout/DashboardLayout";
-import AddNewUser from "./Modals/AddNewUser";
 import { Alert } from "@/components/Alert/Alert";
-function index(): ReactElement {
+import API from '../../../config';
+import { motion } from "framer-motion";
+import { ReactElement, useEffect, useState } from "react";
+import DashboardLayout from "@/components/Layout/DashboardLayout";
+import Cookies from 'js-cookie'
+import { Result } from "antd";
+import LastSeen from "@/components/LastSeen";
+import Link from "next/link";
+import Image from "next/image";
 
-    const [isModalShowen, setIsModalShowen] = useState(false)
-    const setIsModalHiddenHandle = () => {
-        setIsModalShowen(false);
+function index() {
+    const [postsList, setPostsList] = useState([])
+    const [isError, setIsError] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const token = Cookies.get('token_dash')
+
+    const refreshData = async () => {
+        setIsLoading(true)
+        try {
+            const res: any = await API.get('dashboard/users', {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            if (res) {
+                setIsLoading(false)
+                setPostsList(res.data.data)
+                setIsError(false)                
+            }
+        } catch (error) {
+            setIsError(true)
+            setIsLoading(false)
+        }
+    }
+
+    const switchStatus = (status) => {
+        switch (status) {
+            case null:
+                return (<span className="badge bg-warning text-dark">قيد الإنتظار</span>)
+            case 0:
+                return (<span className="badge bg-danger text-light">مرفوضة</span>)
+            case 1:
+                return (<span className="badge bg-success text-light">نشطة</span>)
+        }
+    }
+    useEffect(() => {
+        refreshData()
+    }, [])
+    const catVariants = {
+        visible: (i: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: i * 0.072,
+            },
+        }),
+        hidden: { opacity: 0, y: 9 },
     }
     // Return statement.
     return (
         <>
-            {isModalShowen && <AddNewUser setIsModalHiddenHandle={setIsModalHiddenHandle} />}
             <div className="timlands-panel">
                 <div className="timlands-panel-header d-flex align-items-center">
                     <h2 className="title"><span className="material-icons material-icons-outlined">people</span>إدارة الأعضاء</h2>
                 </div>
+
                 <div className="timlands-table">
                     <table className="table">
                         <thead>
                             <tr>
-                                <th>الاسم واللقب</th>
-                                <th className="hidden-tem">اسم المستخدم</th>
-                                <th>الولاية</th>
-                                <th className="hidden-tem">الحالة</th>
-                                <th className="hidden-tem">متصل</th>
-                                <th>الأدوات</th>
+                                <th>العنوان</th>
+                                <th className="hidden-tem">البريد الإلكتروني</th>
+                                <th className="hidden-tem">تلريخ التسجيل</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {postsList && postsList.map((e: any, i) => (
+                                <motion.tr initial="hidden" variants={catVariants} animate="visible" custom={i} key={e.id}>
+                                    <td>
+                                        <Link href={`/u/${e.username}`}>
+                                            <a className="flex-center">
+                                                <Image src={`${e.profile.avatar_url}`} width={20} height={20} />
+                                                <span className="me-1">{(!e.profile.full_name || e.profile.full_name == '') ? 'بدون اسم' : e.profile.full_name}</span>
+                                            </a>
+                                        </Link>
+                                    </td>
+                                    <td className="hidden-tem">{e.email}</td>
+                                    <td className="hidden-tem" title={e.created_at}><LastSeen date={e.created_at} /></td>
+                                    
+                                </motion.tr>
+                            ))}
+                        </tbody>
                     </table>
-                    <Alert type="warning">
-                        <p className="text"><span className="material-icons">warning_amber</span> لاتوجد أعضاء</p>
-                    </Alert>
+                    {postsList && postsList.length == 0 &&
+                        <Result
+                            status="404"
+                            title="لا يوجد خدمات"
+                            subTitle="في هذه الحالة لاتوجد خدمات"
+                        />}
+
+                    {isError &&
+                        <Alert type="error">
+                            <p className="text"><span className="material-icons">warning_amber</span> حدث خطأ غير متوقع</p>
+                        </Alert>}
+                    {isLoading &&
+                        <motion.div initial={{ opacity: 0, y: 29 }} animate={{ opacity: 1, y: 0 }} className="d-flex py-5 spinner-loader justify-content-center">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </motion.div>
+                    }
                 </div>
             </div>
         </>
