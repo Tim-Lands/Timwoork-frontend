2c154a4dd223c3ff219468fd9c4c12041f5d1923
Timwoork <102923909+Tim-Lands@users.noreply.github.com>
2022-04-08 01:49:13 +0300

    Update Post.tsx
    change url again :)

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import PropTypes from "prop-types";
 import Link from "next/link";
+const APIURL = 'https://api.icoursat.com/products/thumbnails/'
 
 function Post({
     title,
