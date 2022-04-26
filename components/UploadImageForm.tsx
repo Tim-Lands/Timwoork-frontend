e80efc19cfcbfb12329c54c7eb38de90d4a6177f
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-04-26 17:36:41 +0100

    Merge

--- components/UploadImageForm.tsx
+++ components/UploadImageForm.tsx
@@ -2,30 +2,42 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 
-export default function UploadImageForm({ src, validationsErrors, setPicture }: any) {
-    const [imgData, setImgData] = useState(src);
-
+export default function UploadImageForm({validationsErrors, setPicture }: any) {
+    //const [imgData, setImgData] = useState(src);
+    const [images, setImages] = useState([])
+    const [imagesData, setImagesData] = useState([]);
     const onChangePicture = (e: any) => {
         if (e.target.files[0]) {
-            setPicture(e.target.files[0]);
+            setPicture([...images,e.target.files[e.target.files.length-1]]);
+            setImages([...images, e.target.files[e.target.files.length-1]])
             const reader = new FileReader();
             reader.addEventListener("load", () => {
-                setImgData(reader.result);
+                setImagesData([...imagesData,reader.result]);
+            
             });
             reader.readAsDataURL(e.target.files[0]);
         }
 
     };
+
+    const clearImages=()=>{
+        setPicture([])
+        setImages([])
+        setImagesData([])
+    }
     return (
         <div className="login-panel update-form">
             <div className={"panel-modal-body login-panel-body auto-height"}>
+            <button onClick={clearImages}>clear</button>
                 <div className="avatar-uploader image-identifie">
-                    <img width={110} height={110} src={imgData} />
+                    <img width={110} height={110} src={imagesData[0]} />
                     <input id="profilePic" type="file" multiple onChange={onChangePicture} />
                     <button className="upload-btn">
                         <span className="material-icons material-icons-outlined">file_upload</span>
                     </button>
                 </div>
+                {imagesData.slice(1).map((image,index)=><img width={110} src={image} key={index}/>)}
+
             </div>
             {validationsErrors &&
                 <div style={{ overflow: 'hidden' }}>
