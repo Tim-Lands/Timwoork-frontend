be6b913ed7a68c58bc4f7ef25017a32fc0998fef
sharaf eldeen <eldeenshraf@gmail.com>
2022-04-30 18:49:52 +0200

    payment

--- config/index.tsx
+++ config/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: 'https://d165-105-235-129-152.ngrok.io/', //api.timwoork.com
+    baseURL: 'https://api.timwoork.com/', //api.timwoork.com
     withCredentials: true,
 });
