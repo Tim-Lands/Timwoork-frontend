30aaa8755bb7ef7dd7ee0f1e67463c858fb48787
ahmed1622y <aymlsr2001@gmail.com>
2022-05-06 11:16:42 +0200

    product popular pagination

--- pages/products/popular.tsx
+++ pages/products/popular.tsx
@@ -1,69 +1,97 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import Layout from "@/components/Layout/HomeLayout";
 import FilterContent from "../../components/products";
 import { Form, Formik } from "formik";
-import useSWR from "swr";
+import Pagination from "react-js-pagination";
 import Loading from "@/components/Loading";
 import { MetaTags } from "@/components/SEO/MetaTags";
+import Cookies from "js-cookie";
+import API from "../../config";
 
 function Popular() {
-    const [pageIndex, setPageIndex] = useState(1);
-    const { data: getCategories }: any = useSWR(`api/filter?paginate=12&page=${pageIndex}&sort=count_buying,desc`);
-    /**----------------------------------------------------------**/
+  let token = Cookies.get("token");
+  if (!token && typeof window !== "undefined")
+    token = localStorage.getItem("token");
 
-    return (
-        <div className="container py-5">
-            <MetaTags
-                title={'الخدمات الأكثر شعبية'}
-                metaDescription={'الخدمات الأكثر شعبية'}
-                ogDescription={'الخدمات الأكثر شعبية'}
-            />
-            <Formik
-                isInitialValid={true}
-                initialValues={{
-                    categoryID: [],
-                }}
-                onSubmit={async (values) => {
-                    console.log(values);
-                }}
-            >
-                <Form>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="page-header">
-                                <h4 className="title">الخدمات الأكثر شعبية</h4>
-                            </div>
-                            {!getCategories && <Loading />}
-                            <FilterContent
-                                size={3}
-                                products={getCategories && getCategories.data.data}
-                            />
-                            {getCategories &&
-                                getCategories.length !== 0 &&
-                                getCategories.total > getCategories.per_page && (
-                                    <div className="p-2 d-flex">
-                                        <button
-                                            className="btn butt-sm butt-primary me-auto"
-                                            onClick={() => setPageIndex(pageIndex + 1)}
-                                        >
-                                            الصفحة التالية
-                                        </button>
-                                        <button
-                                            className="btn butt-sm butt-primary"
-                                            onClick={() => setPageIndex(pageIndex - 1)}
-                                        >
-                                            الصفحة السابقة
-                                        </button>
-                                    </div>
-                                )}
-                        </div>
-                    </div>
-                </Form>
-            </Formik>
-        </div>
-    );
+  const [getProducts, setGetProducts]: any = useState();
+  //const { data: getProducts }: any = useSWR(`api/filter?paginate=12&sort=count_buying,desc`);
+  /**----------------------------------------------------------**/
+  const fetchData = async (pageNumber: number = 1) => {
+    try {
+      const res = await API.get(
+        `api/filter?paginate=12&page=${pageNumber}&sort=count_buying,desc`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (res.status === 200) {
+        setGetProducts(res.data.data);
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="container py-5">
+      <MetaTags
+        title={"الخدمات الأكثر شعبية"}
+        metaDescription={"الخدمات الأكثر شعبية"}
+        ogDescription={"الخدمات الأكثر شعبية"}
+      />
+      <Formik
+        isInitialValid={true}
+        initialValues={{
+          categoryID: [],
+        }}
+        onSubmit={async (values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="page-header">
+                <h4 className="title">الخدمات الأكثر شعبية</h4>
+              </div>
+              {!getProducts && <Loading />}
+              <FilterContent
+                size={3}
+                products={getProducts && getProducts.data}
+              />
+              {getProducts && (
+                <div>
+                  <hr />
+                  <Pagination
+                    activePage={
+                      getProducts.current_page ? getProducts.current_page : 0
+                    }
+                    itemsCountPerPage={
+                      getProducts.per_page ? getProducts.per_page : 0
+                    }
+                    totalItemsCount={getProducts.total ? getProducts.total : 0}
+                    onChange={(pageNumber) => {
+                      fetchData(pageNumber);
+                    }}
+                    pageRangeDisplayed={8}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    firstPageText={"الصفحة الأولى"}
+                    lastPageText={"الصفحة الأخيرة"}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 Popular.getLayout = function getLayout(page: any): ReactElement {
-    return <Layout>{page}</Layout>;
+  return <Layout>{page}</Layout>;
 };
 export default Popular;
