c91882105fe98f5b0a62ac92cd52c0f5f9c68a6f
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-05-20 14:35:26 +0100

    H

--- pages/tw-admin/users/suspondedspermanent.tsx
+++ pages/tw-admin/users/suspondedspermanent.tsx
@@ -8,10 +8,12 @@ import LastSeen from "@/components/LastSeen";
 import Link from "next/link";
 import Image from "next/image";
 import { Space, Table } from 'antd';
+import SuspensionInfo from "@/components/SuspensionInfo";
 
 function suspondedspermanent() {
     const [postsList, setPostsList] = useState([])
     const [isError, setIsError] = useState(false)
+    const [isShowSuspensionInfo, setIsShowSuspensionInfo] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
 
     const token = Cookies.get('token_dash')
@@ -69,7 +71,15 @@ function suspondedspermanent() {
                             className="btn butt-xs butt-light"
                             type="button"
                         >
-                            إلغاء التعليق
+                            إيقاف التعليق
+                        </button>
+                        <button
+                            title={item.id}
+                            onClick={() => setIsShowSuspensionInfo(true)}
+                            className="btn butt-xs butt-green"
+                            type="button"
+                        >
+                            معلومات التعليق
                         </button>
                     </Space>
                 </>
@@ -105,7 +115,7 @@ function suspondedspermanent() {
                 <div className="timlands-panel-header d-flex align-items-center">
                     <h2 className="title"><span className="material-icons material-icons-outlined">people</span>الحسابات المعلقة دائما</h2>
                 </div>
-
+                {isShowSuspensionInfo && <SuspensionInfo id={4} setIsShowSuspensionInfo={setIsShowSuspensionInfo} />}
                 <Table
                     columns={columns}
                     dataSource={data}
