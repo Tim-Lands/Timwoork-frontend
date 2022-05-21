076590ab46ed28bc58016dba963cb13267d6a1bc
ahmed1622y <aymlsr2001@gmail.com>
2022-05-21 20:26:32 +0200

    change pusher app_key

--- config/pusher.tsx
+++ config/pusher.tsx
@@ -1,17 +1,18 @@
-import Pusher from 'pusher-js'
-import Cookies from 'js-cookie'
+import Pusher from "pusher-js";
+import Cookies from "js-cookie";
 
-let token = Cookies.get('token')
+let token = Cookies.get("token");
 if (!token && typeof window !== "undefined")
-    token = localStorage.getItem('token');
-export const pusher = new Pusher('31365e7905d4a38d6318', {
-    cluster: 'eu',
-    authEndpoint: 'https://api.timwoork.com/api/broadcasting/auth',
-    auth: token ? {
+  token = localStorage.getItem("token");
+export const pusher = new Pusher("a00614632e45ad3d49ff", {
+  cluster: "eu",
+  authEndpoint: "https://api.timwoork.com/api/broadcasting/auth",
+  auth: token
+    ? {
         headers: {
-            // pass the authorization token when using private channels
-            Authorization: `Bearer ${token}`,
+          // pass the authorization token when using private channels
+          Authorization: `Bearer ${token}`,
         },
-    } : undefined,
-
-})
+      }
+    : undefined,
+});
