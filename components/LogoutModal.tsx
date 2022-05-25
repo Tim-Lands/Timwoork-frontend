3b8233365c874d6bef5ad3831336dcbc32446bd5
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-05-25 21:09:12 +0100

    J

--- components/LogoutModal.tsx
+++ components/LogoutModal.tsx
@@ -38,21 +38,23 @@ function LogoutModal({ setIsLogoutModal }): ReactElement {
     }
     return (
         <motion.div initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} className='modal-conferm'>
-            <div className="modal-conferm-head">
-                <h3 className="title">
-                    رسالة تأكيد
-                </h3>
-            </div>
-            <div className="modal-conferm-body">
-                <p className="text">
-                    هل أنت متأكد من أنك تريد تسجيل الخروج من جميع الأجهزة؟
-                </p>
-            </div>
-            <div className="modal-conferm-footer">
-                <Space>
-                    <button className='btn butt-sm butt-green' onClick={logout}>نعم</button>
-                    <button className='btn butt-sm butt-red-text' onClick={() => setIsLogoutModal(false)}>لا</button>
-                </Space>
+            <div className="modal-conferm-inner">
+                <div className="modal-conferm-head">
+                    <h3 className="title">
+                        رسالة تأكيد
+                    </h3>
+                </div>
+                <div className="modal-conferm-body">
+                    <p className="text">
+                        هل أنت متأكد من أنك تريد تسجيل الخروج من جميع الأجهزة؟
+                    </p>
+                </div>
+                <div className="modal-conferm-footer">
+                    <Space>
+                        <button className='btn butt-sm butt-green' onClick={logout}>نعم</button>
+                        <button className='btn butt-sm butt-red-text' onClick={() => setIsLogoutModal(false)}>لا</button>
+                    </Space>
+                </div>
             </div>
         </motion.div>
     )
