712be606eb0d21d35b7a9ac8fcfb20041eb48dd8
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-05-27 21:19:41 +0100

    SE

--- components/ReplyContactModal.tsx
+++ components/ReplyContactModal.tsx
@@ -2,16 +2,123 @@ import { Space } from 'antd'
 import React, { ReactElement, useState } from 'react'
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
+export const MenuBar = ({ editor }) => {
+    if (!editor) {
+        return null;
+    }
+
+    return (
+        <div className="menubar">
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                className={editor.isActive("bold") ? "is-active" : ""}
+            >
+                <span className="material-icons material-icons-outlined">
+                    format_bold
+                </span>
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                className={editor.isActive("italic") ? "is-active" : ""}
+            >
+                <span className="material-icons material-icons-outlined">
+                    format_italic
+                </span>
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                className={editor.isActive("strike") ? "is-active" : ""}
+            >
+                <span className="material-icons material-icons-outlined">
+                    strikethrough_s
+                </span>
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+                className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+            >
+                h1
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+                className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+            >
+                h2
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+                className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+            >
+                h3
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+                className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
+            >
+                h4
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleBulletList().run()}
+                className={editor.isActive("bulletList") ? "is-active" : ""}
+            >
+                <span className="material-icons material-icons-outlined">
+                    format_list_bulleted
+                </span>
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().toggleOrderedList().run()}
+                className={editor.isActive("orderedList") ? "is-active" : ""}
+            >
+                <span className="material-icons material-icons-outlined">
+                    format_list_numbered
+                </span>
+            </button>
+            <button
+                type="button"
+                onClick={() => editor.chain().focus().setHorizontalRule().run()}
+            >
+                <span className="material-icons material-icons-outlined">
+                    horizontal_rule
+                </span>
+            </button>
+        </div>
+    );
+};
+const Tiptap = (props: any) => {
+    return (
+        <EditorContent
+            content={props.value}
+            editor={props.editor}
+            onChange={props.changeHandle}
+            style={{ minHeight: 170 }}
+        />
+    );
+};
 function ReplyContactModal({ setIsConfirmText, handleFunc, title }): ReactElement {
     const [fromState, setFromState] = useState('')
     const [toState, setToState] = useState('')
     const [messageState, setMessageState] = useState('')
-    
-    const [validationsErrors, setValidationsErrors]: any = useState({});
+    const [subject, setSubject] = useState('')
 
+    const [validationsErrors, setValidationsErrors]: any = useState({});
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: 'stars && stars.data.buyer_instruct',
+    });
     return (
-        <motion.div initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} className='modal-conferm'>
+        <motion.div initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1 }} className='modal-conferm lg'>
             <div className="modal-conferm-inner">
                 <div className="modal-conferm-head">
                     <h3 className="title" style={{ fontSize: 17 }}>
@@ -19,103 +126,134 @@ function ReplyContactModal({ setIsConfirmText, handleFunc, title }): ReactElemen
                     </h3>
                 </div>
                 <div className="modal-conferm-body" style={{ paddingTop: 0 }}>
-                    <div className="timlands-form">
-                        <label className="label-block" htmlFor="input-msg">
-                            من
-                        </label>
-                        <input
-                            type='text'
-                            id="input-msg"
-                            name="fromState"
-                            placeholder="من..."
-                            className={
-                                "timlands-inputs sm " +
-                                (validationsErrors &&
-                                    validationsErrors.fromState &&
-                                    " has-error")
-                            }
-                            onChange={e => setFromState(e.target.value)}
-                            value={fromState}
-                        />
-                        {validationsErrors && validationsErrors.fromState && (
-                            <div style={{ overflow: "hidden" }}>
-                                <motion.div
-                                    initial={{ y: -70, opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    className="timlands-form-note form-note-error"
-                                >
-                                    <p className="text">
-                                        {validationsErrors.fromState[0]}
-                                    </p>
-                                </motion.div>
+                    <div className="row">
+                        <div className="col-xl-6">
+                            <div className="timlands-form">
+                                <label className="label-block" htmlFor="input-msg">
+                                    من
+                                </label>
+                                <input
+                                    type='text'
+                                    id="input-msg"
+                                    name="fromState"
+                                    placeholder="من..."
+                                    className={
+                                        "timlands-inputs sm " +
+                                        (validationsErrors &&
+                                            validationsErrors.fromState &&
+                                            " has-error")
+                                    }
+                                    onChange={e => setFromState(e.target.value)}
+                                    value={fromState}
+                                />
+                                {validationsErrors && validationsErrors.fromState && (
+                                    <div style={{ overflow: "hidden" }}>
+                                        <motion.div
+                                            initial={{ y: -70, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            className="timlands-form-note form-note-error"
+                                        >
+                                            <p className="text">
+                                                {validationsErrors.fromState[0]}
+                                            </p>
+                                        </motion.div>
+                                    </div>
+                                )}
                             </div>
-                        )}
-                    </div>
-                    <div className="timlands-form">
-                        <label className="label-block" htmlFor="input-toState">
-                            إلى
-                        </label>
-                        <input
-                            type='text'
-                            id="input-toState"
-                            name="toState"
-                            placeholder="إلى..."
-                            className={
-                                "timlands-inputs sm " +
-                                (validationsErrors &&
-                                    validationsErrors.toState &&
-                                    " has-error")
-                            }
-                            onChange={e => setToState(e.target.value)}
-                            value={toState}
-                        />
-                        {validationsErrors && validationsErrors.toState && (
-                            <div style={{ overflow: "hidden" }}>
-                                <motion.div
-                                    initial={{ y: -70, opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    className="timlands-form-note form-note-error"
-                                >
-                                    <p className="text">
-                                        {validationsErrors.toState[0]}
-                                    </p>
-                                </motion.div>
+                        </div>
+                        <div className="col-xl-6">
+                            <div className="timlands-form">
+                                <label className="label-block" htmlFor="input-toState">
+                                    إلى
+                                </label>
+                                <input
+                                    type='text'
+                                    id="input-toState"
+                                    name="toState"
+                                    placeholder="إلى..."
+                                    className={
+                                        "timlands-inputs sm " +
+                                        (validationsErrors &&
+                                            validationsErrors.toState &&
+                                            " has-error")
+                                    }
+                                    onChange={e => setToState(e.target.value)}
+                                    value={toState}
+                                />
+                                {validationsErrors && validationsErrors.toState && (
+                                    <div style={{ overflow: "hidden" }}>
+                                        <motion.div
+                                            initial={{ y: -70, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            className="timlands-form-note form-note-error"
+                                        >
+                                            <p className="text">
+                                                {validationsErrors.toState[0]}
+                                            </p>
+                                        </motion.div>
+                                    </div>
+                                )}
                             </div>
-                        )}
-                    </div>
-                    <div className="timlands-form">
-                        <label className="label-block" htmlFor="input-messageState">
-                            نص الرسالة
-                        </label>
-                        <textarea
-                            id="input-messageState"
-                            name="messageState"
-                            placeholder="نص الرسالة..."
-                            className={
-                                "timlands-inputs sm " +
-                                (validationsErrors &&
-                                    validationsErrors.messageState &&
-                                    " has-error")
-                            }
-                            onChange={e => setMessageState(e.target.value)}
-                            value={messageState}
-                            style={{
-                                minHeight: 140
-                            }}
-                        />
-                        {validationsErrors && validationsErrors.messageState && (
-                            <div style={{ overflow: "hidden" }}>
-                                <motion.div
-                                    initial={{ y: -70, opacity: 0 }}
-                                    animate={{ y: 0, opacity: 1 }}
-                                    className="timlands-form-note form-note-error"
-                                >
-                                    <p className="text">
-                                        {validationsErrors.messageState[0]}
-                                    </p>
-                                </motion.div>
+                        </div>
+                        <div className="col-xl-12">
+                            <div className="timlands-form">
+                                <label className="label-block" htmlFor="input-subject">
+                                    موضوع الرسالة
+                                </label>
+                                <input
+                                    type='text'
+                                    id="input-subject"
+                                    name="subject"
+                                    placeholder="موضوع الرسالة..."
+                                    className={
+                                        "timlands-inputs sm " +
+                                        (validationsErrors &&
+                                            validationsErrors.subject &&
+                                            " has-error")
+                                    }
+                                    onChange={e => setSubject(e.target.value)}
+                                    value={subject}
+                                />
+                                {validationsErrors && validationsErrors.subject && (
+                                    <div style={{ overflow: "hidden" }}>
+                                        <motion.div
+                                            initial={{ y: -70, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            className="timlands-form-note form-note-error"
+                                        >
+                                            <p className="text">
+                                                {validationsErrors.subject[0]}
+                                            </p>
+                                        </motion.div>
+                                    </div>
+                                )}
                             </div>
-                        )}
+                        </div>
+                        <div className="col-xl-12">
+                            <div className="timlands-form">
+                                <div className="app-content-editor">
+                                    <MenuBar editor={editor} />
+                                    <Tiptap
+                                        value={messageState}
+                                        changeHandle={(e: any) => setMessageState(e.target.value)}
+                                        editor={editor}
+                                    />
+                                </div>
+                                {validationsErrors && validationsErrors.messageState && (
+                                    <div style={{ overflow: "hidden" }}>
+                                        <motion.div
+                                            initial={{ y: -70, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            className="timlands-form-note form-note-error"
+                                        >
+                                            <p className="text">
+                                                {validationsErrors.messageState[0]}
+                                            </p>
+                                        </motion.div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -125,7 +263,7 @@ function ReplyContactModal({ setIsConfirmText, handleFunc, title }): ReactElemen
                         <button
                             className='btn butt-sm butt-red-text'
                             onClick={() => {
-                                setIsConfirmText(false)  
+                                setIsConfirmText(false)
                                 setValidationsErrors() // just Test
                             }}
                         >إلغاء الأمر</button>
