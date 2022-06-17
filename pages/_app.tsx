8cf36b88d5a8387707d70c356a7ff017786e1c1e
Abdelhamid Boumegouas <aboumegouass@gmail.com>
2022-06-17 22:40:46 +0100

    Lo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import i18n from "i18next";
 import "antd/dist/antd.css";
 import "../styles/app-rtl.css";
 import store from "@/store/store";
 import { Provider } from "react-redux";
 import PropTypes from "prop-types";
 //import useSWR from "swr";
-import { useEffect } from "react";
 import type { ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
@@ -21,12 +19,6 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  useEffect(() => {
-    const tt: string = i18n.dir();
-    if (tt !== "rtl") {
-      import("../styles/app" + ".css");
-    }
-  }, []);
   const getLayout = Component.getLayout ?? ((page: any) => page);
   return (
     <div>
