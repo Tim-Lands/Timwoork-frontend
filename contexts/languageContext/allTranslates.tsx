bf3b79e01998093fde5e49c1f46a126eadf68f5b
ahmed1622y <aymlsr2001@gmail.com>
2022-06-20 05:31:04 +0200

    0.1

--- contexts/languageContext/allTranslates.tsx
+++ contexts/languageContext/allTranslates.tsx
@@ -1,5 +1,30 @@
 import blog from "../../translates/blog.json";
 import main from "../../translates/main.json";
+import add_new from "../../translates/add_new.json";
+import auth from "../../translates/auth.json";
+import cart from "../../translates/cart.json";
+import categories from "../../translates/categories.json";
+import club from "../../translates/club.json";
+import conversion from "../../translates/conversion.json";
+import my_wallet from "../../translates/my_wallet.json";
+import notification from "../../translates/notification.json";
+import products from "../../translates/products.json";
+import soon from "../../translates/soon.json";
+import contact_us from "../../translates/contact_us.json";
 
-const translates = { main, blog };
+const translates = {
+  main,
+  blog,
+  add_new,
+  contact_us,
+  club,
+  cart,
+  categories,
+  auth,
+  conversion,
+  soon,
+  products,
+  notification,
+  my_wallet,
+};
 export default translates;
