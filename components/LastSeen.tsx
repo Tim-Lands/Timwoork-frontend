51cebfb1e4af8111571c2c03e7de135a32cc8e45
ahmed1622y <aymlsr2001@gmail.com>
2022-06-28 17:21:27 +0200

    try

--- components/LastSeen.tsx
+++ components/LastSeen.tsx
@@ -1,31 +1,28 @@
-import TimeAgo from "react-timeago";
-import fr from "react-timeago/lib/language-strings/fr";
-import en from "react-timeago/lib/language-strings/en";
-import ar from "react-timeago/lib/language-strings/ar";
-import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
-import { useContext } from "react";
-import { LanguageContext } from "../contexts/languageContext/context";
+// import TimeAgo from "react-timeago";
+// import fr from "react-timeago/lib/language-strings/fr";
+// import en from "react-timeago/lib/language-strings/en";
+// import ar from "react-timeago/lib/language-strings/ar";
+// import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+// import { useContext } from "react";
+// import { LanguageContext } from "../contexts/languageContext/context";
 import PropTypes from "prop-types";
 
 export default function LastSeen({ date }) {
-  const { language } = useContext(LanguageContext);
-  const formatter = buildFormatter(which(language, ar, en, fr));
-  return (
-    <span>
-      <TimeAgo date={date} formatter={formatter} />
-    </span>
-  );
+  console.log(date);
+  // const { language } = useContext(LanguageContext);
+  // const formatter = buildFormatter(which(language, ar, en, fr));
+  return <span>{/* <TimeAgo date={date} formatter={formatter} /> */}</span>;
 }
-const which = (language, ar, en, fr) => {
-  switch (language) {
-    case "ar":
-      return ar;
-    case "en":
-      return en;
-    case "fr":
-      return fr;
-  }
-};
+// const which = (language, ar, en, fr) => {
+//   switch (language) {
+//     case "ar":
+//       return ar;
+//     case "en":
+//       return en;
+//     case "fr":
+//       return fr;
+//   }
+// };
 LastSeen.propTypes = {
   date: PropTypes.any,
 };
