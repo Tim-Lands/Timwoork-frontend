3d835d3923f46c1e3d363bbf298191ed516a2fb0
shraf <eldeenshraf@gmail.com>
2023-01-12 02:55:31 +0200

    enhancing interface

--- services/webRTC/index.ts
+++ services/webRTC/index.ts
@@ -3,28 +3,38 @@ export class WebRTCService {
     private static instance: WebRTCService;
     private peer;
     private stream;
-    private constructor() {
+    private signal;
+
+    private constructor(signal: any) {
+
         console.log("constructing webRTC service ");
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((currentStream) => {
                 this.stream = currentStream;
             });
-        this.peer = new Peer({ initiator: false, trickle: false, stream:this.stream });
+        this.peer = new Peer({ initiator: false, trickle: false, stream: this.stream });
+        if (signal)
+            this.peer.signal(signal)
     }
 
-    public static async Instance() {
+    public static async Instance(signal?: any) {
         if (!this.instance) {
-          this.instance = new this();
+            if (signal)
+                this.instance = new this(signal);
+            else
+                return null
         }
         return this.instance;
-      }
-
-    public onSignal(callback:Function){
-        callback()
     }
 
-    public onStream(callback:Function){
-        callback(this.stream)
+
+
+    public onSignal(callback: Function) {
+        this.peer.on('signal',callback)
     }
-    
+
+    public onStream(callback: Function) {
+       this.peer.on('stream',callback)
+    }
+
 }
