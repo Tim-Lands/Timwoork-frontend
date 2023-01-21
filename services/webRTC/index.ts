6af624d09ab90c3b899868b58a074c291606c89f
shraf <eldeenshraf@gmail.com>
2023-01-21 12:46:19 +0200

    edit in webrtc service

--- services/webRTC/index.ts
+++ services/webRTC/index.ts
@@ -1,44 +1,71 @@
-import Peer from 'simple-peer';
 export class WebRTCService {
     private static instance: WebRTCService;
-    private peer;
-    private stream;
-    private signal;
+    private device
+    private rtpCapabilities
+    private producerTransport
+    private consumerTransport
+    private producer
+    private consumer
+
+    private params: any = {
+        // mediasoup params
+        encodings: [
+            {
+                rid: 'r0',
+                maxBitrate: 100000,
+                scalabilityMode: 'S1T3',
+            },
+            {
+                rid: 'r1',
+                maxBitrate: 300000,
+                scalabilityMode: 'S1T3',
+            },
+            {
+                rid: 'r2',
+                maxBitrate: 900000,
+                scalabilityMode: 'S1T3',
+            },
+        ],
+        // https://mediasoup.org/documentation/v3/mediasoup-client/api/#ProducerCodecOptions
+        codecOptions: {
+            videoGoogleStartBitrate: 1000
+        }
+    }
+
+    public getLocalStream(onStreamSuccess: Function) {
+        navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                width: {
+                    min: 640,
+                    max: 1920,
+                },
+                height: {
+                    min: 400,
+                    max: 1080,
+                }
+            }
+        }).then(stream => {
+            onStreamSuccess(stream);
+            this.params = {
+                track: stream.getVideoTracks()[0],
+                ...this.params
+            }
+        })
+
+    }
+
+
 
     private constructor(signal: any) {
 
-        console.log("constructing webRTC service ");
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-            .then((currentStream) => {
-                this.stream = currentStream;
-            });
-        this.peer = new Peer({ initiator: false, trickle: false, stream: this.stream });
-        this.peer.signal(signal)
-        this.signal = signal
+
     }
 
     public static async Instance(signal?: any) {
-        if (!this.instance) {
-            if (signal)
-                this.instance = new this(signal);
-            else
-                return null
-        }
+
         return this.instance;
     }
 
-
-
-    public onSignal(callback: Function) {
-        this.peer.on('signal',callback)
-    }
-
-    public onStream(callback: Function) {
-       this.peer.on('stream',callback)
-    }
-
-    public getSignal(){
-        return this.signal
-    }
 
 }
