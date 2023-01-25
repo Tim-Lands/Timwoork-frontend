3602141f0051d1b7bbc837085280452547cfb25d
ahmed1622y <aymlsr2001@gmail.com>
2023-01-25 10:58:10 +0200

    return meta tags translates

--- components/SEO/MetaTags.tsx
+++ components/SEO/MetaTags.tsx
@@ -11,7 +11,7 @@
 import Head from "next/head";
 import PropTypes from "prop-types";
 import { ReactElement } from "react";
-// import { useAppSelector } from "@/store/hooks";
+import { useAppSelector } from "@/store/hooks";
 export function MetaTags({
   title,
   metaDescription,
@@ -20,37 +20,37 @@ export function MetaTags({
   ogImage,
   ogUrl,
 }): ReactElement {
-  // const { getAll, language } = useAppSelector((state) => state.languages);
+  const { getAll, language } = useAppSelector((state) => state.languages);
 
   return (
-    <>
-      <Head>
-        <meta name="description" content={metaDescription} key="description" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} key="title" />
-        <meta property="og:site_name" content="موقع تيم ورك" />
-        <meta property="og:locale" content="ar" />
-        <meta property="og:locale:alternate" content="ar" />
-        <meta property="og:description" content={ogDescription} />
-        {keywords && (
-          <meta
-            name="keywords"
-            content={keywords.map((keyword: any) => keyword.name + ", ")}
-          />
-        )}
-        {/* Twitter cards */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@timwoorkDotCom" />
-        <meta name="twitter:creator" content="@timwoorkDotCom" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={ogDescription} />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <title>تيموورك | {title}</title>
-        {ogImage && <meta property="og:image" content={ogImage} key="image" />}
-        {ogUrl && <meta property="og:url" content={ogUrl} />}
-      </Head>
-    </>
+    <Head>
+      <meta name="description" content={metaDescription} key="description" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} key="title" />
+      <meta property="og:site_name" content={getAll("Timwoork_website")} />
+      <meta property="og:locale" content={language} />
+      <meta property="og:locale:alternate" content={language} />
+      <meta property="og:description" content={ogDescription} />
+      {keywords && (
+        <meta
+          name="keywords"
+          content={keywords.map((keyword: any) => keyword.name + ", ")}
+        />
+      )}
+      {/* Twitter cards */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@timwoorkDotCom" />
+      <meta name="twitter:creator" content="@timwoorkDotCom" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={ogDescription} />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <title>
+        {getAll("Logo")} | {title}
+      </title>
+      {ogImage && <meta property="og:image" content={ogImage} key="image" />}
+      {ogUrl && <meta property="og:url" content={ogUrl} />}
+    </Head>
   );
 }
 MetaTags.propTypes = {
