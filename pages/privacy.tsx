d8aeb1f80e3cce6d177264de778f974d21eab001
shraf <eldeenshraf@gmail.com>
2023-01-26 14:12:04 +0200

    test seo

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -8,7 +8,7 @@ function Privacy() {
   const { getAll } = useAppSelector((state) => state.languages);
 
   return (
-    <div className="py-4">
+    <>
       <NextSeo
         title="Your Title"
         description="This is a demo description"
@@ -43,57 +43,59 @@ function Privacy() {
           cardType: "summary_large_image",
         }}
       />
-      <MetaTags
-        title={getAll("Privacy_policy")}
-        metaDescription={getAll("Arabic_text_you")}
-        ogDescription={getAll("Arabic_text_you")}
-      />
-      <div className="container my-3">
-        <div className="bg-white p-2 px-4">
-          <div className="page-header">
-            <h2 className="title">{getAll("Privacy_policy")}</h2>
-          </div>
-          <div className="page-content">
-            <p className="text">{getAll("The_Website’s_administration")}</p>
-          </div>
-          <div className="page-content">
-            <h2 className="sub-title">{getAll("Disclaimer_of_legal")}</h2>
-            <p className="text">{getAll("The_Website’s_administration")}</p>
-          </div>
-          <div className="page-content">
-            <h2 className="sub-title">{getAll("Cases_of_service")}</h2>
-            <p className="text">{getAll("The_Website’s_management")}</p>
-          </div>
-          <div className="page-content">
-            <h2 className="sub-title">
-              {getAll("Subscriber’s_account_password")}
-            </h2>
+      <div className="py-4">
+        <MetaTags
+          title={getAll("Privacy_policy")}
+          metaDescription={getAll("Arabic_text_you")}
+          ogDescription={getAll("Arabic_text_you")}
+        />
+        <div className="container my-3">
+          <div className="bg-white p-2 px-4">
+            <div className="page-header">
+              <h2 className="title">{getAll("Privacy_policy")}</h2>
+            </div>
+            <div className="page-content">
+              <p className="text">{getAll("The_Website’s_administration")}</p>
+            </div>
+            <div className="page-content">
+              <h2 className="sub-title">{getAll("Disclaimer_of_legal")}</h2>
+              <p className="text">{getAll("The_Website’s_administration")}</p>
+            </div>
+            <div className="page-content">
+              <h2 className="sub-title">{getAll("Cases_of_service")}</h2>
+              <p className="text">{getAll("The_Website’s_management")}</p>
+            </div>
+            <div className="page-content">
+              <h2 className="sub-title">
+                {getAll("Subscriber’s_account_password")}
+              </h2>
 
-            <ul className="list">
-              <li>{getAll("The_subscriber_chooses")}</li>
-              <li>{getAll("The_subscriber_takes")}</li>
-              <li>{getAll("The_subscriber_takes_2")}</li>
-              <li>{getAll("The_subscriber_must")}</li>
-              <li>{getAll("It_is_prohibited")}</li>
-              <li>{getAll("It_is_strictly")}</li>
-            </ul>
-          </div>
-          <div className="page-content">
-            <h2 className="sub-title">التسجيل</h2>
-            <p className="text">{getAll("Timwoork’s_website")}</p>
-          </div>
-          <div className="page-content">
-            <h2 className="sub-title">{getAll("Content_censorship")}</h2>
-            <p className="text">
-              {getAll("Timwoork’s_management_reserves")}
-              {getAll("All_contents_of")}
-            </p>
-            <br></br>
-            <p className="text">{getAll("This_policy_is")}</p>
+              <ul className="list">
+                <li>{getAll("The_subscriber_chooses")}</li>
+                <li>{getAll("The_subscriber_takes")}</li>
+                <li>{getAll("The_subscriber_takes_2")}</li>
+                <li>{getAll("The_subscriber_must")}</li>
+                <li>{getAll("It_is_prohibited")}</li>
+                <li>{getAll("It_is_strictly")}</li>
+              </ul>
+            </div>
+            <div className="page-content">
+              <h2 className="sub-title">التسجيل</h2>
+              <p className="text">{getAll("Timwoork’s_website")}</p>
+            </div>
+            <div className="page-content">
+              <h2 className="sub-title">{getAll("Content_censorship")}</h2>
+              <p className="text">
+                {getAll("Timwoork’s_management_reserves")}
+                {getAll("All_contents_of")}
+              </p>
+              <br></br>
+              <p className="text">{getAll("This_policy_is")}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 Privacy.getLayout = function getLayout(page: any): ReactElement {
