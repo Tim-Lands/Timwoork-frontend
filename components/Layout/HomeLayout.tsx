b7d7eb2f09405a4d117524ff144c5f68d5d9e01f
shraf <eldeenshraf@gmail.com>
2023-01-26 14:44:43 +0200

    test seo

--- components/Layout/HomeLayout.tsx
+++ components/Layout/HomeLayout.tsx
@@ -4,6 +4,7 @@ import router from "next/router";
 import { useEffect, useState } from "react";
 import Footer from "../Footer/Footer";
 import { useAppSelector } from "@/store/hooks";
+import { NextSeo } from "next-seo";
 
 function Layout(props: any) {
   const [loading, setLoading] = useState(false);
@@ -19,6 +20,46 @@ function Layout(props: any) {
     router.events.on("routeChangeError", handleComplete);
   }, [router]);
   return (
+    <>
+    <NextSeo
+        title="Your Title"
+        description="This is a demo description"
+        canonical="https://www.google.com"
+        openGraph={{
+          url: "https://www.example.com",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/play_10_year-keyword_asset-.png",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+            {
+              url: "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/play_10_year-keyword_asset-.png",
+
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+              type: "image/jpeg",
+            },
+            {
+              url: "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/play_10_year-keyword_asset-.png",
+            },
+            {
+              url: "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/play_10_year-keyword_asset-.png",
+            },
+          ],
+          site_name: "YourSiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
     <div className="pt-5 mainHomeIndex">
       <Navbar />
       {props.children}
@@ -29,6 +70,7 @@ function Layout(props: any) {
       )}
       <Footer />
     </div>
+    </>
   );
 }
 export default Layout;
