67863a4b4afaef283aeb64845bcbb0db82f51b62
shraf <eldeenshraf@gmail.com>
2023-01-26 15:34:09 +0200

    test meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,6 +45,7 @@ export default class CustomDocument extends Document {
             }}
           />
           {/* <title>تيموورك</title> */}
+          <meta name="discription" content="sharaf eldeeen"/>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
