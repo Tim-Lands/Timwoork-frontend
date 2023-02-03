453b32615d7ea389d64a847d9fb25d2c4a8c685f
ahmed1622y <aymlsr2001@gmail.com>
2023-02-03 16:44:58 +0200

    restore new seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ import PropTypes from "prop-types";
 import { ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import InnerApp from "../components/_innerApp";
 type NextPageWithLayout = NextPage & {
   getLayout?: () => ReactNode;
@@ -27,6 +28,42 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: any) => page);
   return (
     <div>
+      <Head>
+        <meta
+          name="description"
+          content={"موقع تيم ورك العالمي للعمل الحر"}
+          key="description"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={"موقع تيم ورك "} key="title" />
+        <meta property="og:site_name" content={"موقع تيم ورك"} />
+        <meta property="og:locale" content={"ar"} />
+        <meta property="og:locale:alternate" content={"ar"} />
+        <meta
+          property="og:description"
+          content={"موقع تيم ورك العالمي للعمل الحر"}
+        />
+
+        {/* Twitter cards */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@timwoorkDotCom" />
+        <meta name="twitter:creator" content="@timwoorkDotCom" />
+        <meta name="twitter:title" content={"موقع تيم ورك "} />
+        <meta
+          name="twitter:description"
+          content={"موقع تيم ورك العالمي للعمل الحر"}
+        />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <title>{"موقع تيم ورك "}</title>
+        <meta
+          property="og:image"
+          content={"https://timwoork.com/1.png"}
+          key="image"
+        />
+        <meta property="og:url" content={"https://timwoork.com/"} />
+      </Head>
+
       <Provider store={store}>
         <InnerApp innerApp={getLayout(<Component {...pageProps} />)} />
       </Provider>
